3d3c012bfdbdc7ebacafe2b002a80b298090df41
arv4n <godviona@gmail.com>
2025-04-21 22:58:29 +0700

    daily use

---
+++ brenda/Bar.bash
@@ -0,0 +1,3 @@
+for mon in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$mon polybar -q brenda -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
+done
---
+++ brenda/theme-config.bash
@@ -0,0 +1,98 @@
+
+bg="#141b1e"
+fg="#d3c6aa"
+
+black="#475258"
+red="#e67e80"
+green="#a7c080"
+yellow="#dbbc7f"
+blue="#7fbbb3"
+magenta="#d699b6"
+cyan="#83c092"
+white="#d3c6aa"
+blackb="#475258"
+redb="#e67e80"
+greenb="#a7c080"
+yellowb="#dbbc7f"
+blueb="#7fbbb3"
+magentab="#d699b6"
+cyanb="#83c092"
+whiteb="#d3c6aa"
+
+accent_color="#272e33"
+arch_icon="#0f94d2"
+
+# Bspwm options
+BORDER_WIDTH="0"		# Bspwm border
+TOP_PADDING="48"
+BOTTOM_PADDING="1"
+LEFT_PADDING="1"
+RIGHT_PADDING="1"
+NORMAL_BC="#141A1E"		# Normal border color
+FOCUSED_BC="#a7c080"	# Focused border color
+
+# Terminal font & size
+term_font_size="11"
+term_font_name="JetBrainsMono Nerd Font"
+
+# Picom options
+P_FADE="true"			# Fade true|false
+P_SHADOWS="true"		# Shadows true|false
+SHADOW_C="#000000"		# Shadow color
+P_CORNER_R="6"			# Corner radius (0 = disabled)
+P_BLUR="true"			# Blur true|false
+P_ANIMATIONS="@"		# (@ = enable) (# = disable)
+P_TERM_OPACITY="0.92"	# Terminal transparency. Range: 0.1 - 1.0 (1.0 = disabled)
+
+# Dunst
+dunst_offset='(20, 60)'
+dunst_origin='top-right'
+dunst_transparency='0'
+dunst_corner_radius='6'
+dunst_font='JetBrainsMono NF Medium 9'
+dunst_border='0'
+dunst_frame_color="$green"
+dunst_icon_theme="Gruvbox-Plus-Dark"
+# Dunst animations
+dunst_close_preset="fly-out"
+dunst_close_direction="right"
+dunst_open_preset="fly-in"
+dunst_open_direction="right"
+
+# Jgmenu colors
+jg_bg="$bg"
+jg_fg="$fg"
+jg_sel_bg="$black"
+jg_sel_fg="$fg"
+jg_sep="$green"
+
+# Rofi menu font and colors
+rofi_font="JetBrainsMono NF Bold 9"
+rofi_background="$bg"
+rofi_bg_alt="$accent_color"
+rofi_background_alt="${bg}E0"
+rofi_fg="$fg"
+rofi_selected="$blue"
+rofi_active="$green"
+rofi_urgent="$red"
+
+# Screenlocker
+sl_bg="${bg:1}"
+sl_fg="${fg:1}"
+sl_ring="${bg:1}"
+sl_wrong="${red:1}"
+sl_date="${fg:1}"
+sl_verify="${green:1}"
+
+# Gtk theme
+gtk_theme="Everforest-zk"
+gtk_icons="Gruvbox-Plus-Dark"
+gtk_cursor="Qogirr-Dark"
+geany_theme="z0mbi3-EverForest"
+
+# Wallpaper engine
+
+ENGINE="Theme"
+CUSTOM_DIR="/path/to/dir"
+CUSTOM_WALL="/path/to/image"
+CUSTOM_ANIMATED="$HOME/.config/bspwm/src/assets/animated_wall.mp4"
